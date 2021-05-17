bd67144f6dcd56f5f9124d95c994124d345e0b14
Dmitry <dima.arapov@gmail.com>
2021-05-17 23:30:54 +0300

    Telegram notifications beta

---
+++ src/core/telegram/telegram-data-constructor.ts
@@ -0,0 +1,73 @@
+import {ItemStatus, StateItem} from "#src/model/task";
+import pjson from "#src/../package.json";
+import {WEBHOOK_CHANGED_COLOR, WEBHOOK_NEW_COLOR} from "#src/config";
+
+const bold = (str: string): string => `**${str}**`;
+
+const striked = (str: string): string => `~~${str}~~`;
+
+const telegramDataConstructor = (
+  stateItem: StateItem
+): string => {
+  const {
+    status,
+    valuesChanged = [],
+    listLink
+  } = stateItem;
+  const {
+    title = ``,
+    price = ``,
+    photoLink = ``,
+    geoReferences = ``,
+    link = ``
+  } = stateItem.info;
+
+  const typeTag = ({
+    [ItemStatus.NEW]: `New`,
+    [ItemStatus.CHANGED]: `Changed`,
+  }[status]);
+
+
+  let text = ``;
+
+  text += `[${
+    title.replace(/Объявление/, ``) + ` - ${price}`
+  }](${
+    link
+  })` + `\n`;
+  text += `\n`;
+
+  text += `${
+    (status === ItemStatus.NEW)
+      ? `❗️`
+      : `✏`
+  } ${bold(`Type`)}` + `\n`;
+  text += `#${typeTag}` + `\n`;
+  text += `\n`;
+
+  if (price) {
+    text += `💰 ${bold(`Price`)}` + `\n`;
+
+    const changedInfo = valuesChanged.find((item) => item.key === `price`);
+    if (!changedInfo) {
+      text += price + `\n`;
+    } else {
+      text += `${bold(price)} (${striked(changedInfo.previousValue)})` + `\n`;
+    }
+
+    text += `\n`;
+  }
+
+  if (geoReferences) {
+    text += `📍 ${bold(`Geo`)}` + `\n`;
+    text += geoReferences + `\n`;
+
+    text += `\n`;
+  }
+  text += `🔗 ${bold(`Links`)}` + `\n`;
+  text += `[All results](${listLink}) | [Direct link](${link})` + `\n`;
+
+  return text;
+};
+
+export default telegramDataConstructor;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import {readConsole} from "./core/util/read-console";
 import chalk from "chalk";
 import gainLinks from "./core/gain-links";
-import Task from "./model/task";
+import Task, {StateItem} from "./model/task";
 import {waitFor} from "#src/core/util/wait-for";
 import sendOnWebhook from "#src/core/discord/send-on-webhook";
 import webhookDataConstructor from "#src/core/discord/webhook-data-constructor";
@@ -9,94 +8,116 @@ import checkMemory from "#src/core/check-memory";
 import pjson from "#src/../package.json";
 import readProxyList from "#src/core/proxy/read-proxy-list";
 import {IpBanError, TimeoutError} from "#src/core/errors";
+import TelegramClient from "#src/core/telegram/telegram-client";
 
 require(`dotenv`).config();
 
 (async () => {
-  console.log(`- Avito Monitor v${pjson.version} -`);
+    console.log(`- Avito Monitor v${pjson.version} -`);
 
-  // Найдем ссылки
-  let links;
-  try {
-    links = await gainLinks();
-  } catch (e) {
-    throw new Error(`Check .env file`);
-  }
-
-  console.log();
-  // Поиск прокси
-
-  let proxy;
-  try {
-    proxy = await readProxyList(process.env.PROXY);
-  } catch (e) {
-    console.log(chalk.yellow(`-@@ No proxy list loaded`));
-  }
-
-
-  const task = new Task({
-    links,
-    proxy,
-    subscribe: {
-      onNew: (item) => {
-        if (process.env.WEBHOOK_ON_NEW !== `false`) {
-          sendOnWebhook(webhookDataConstructor(item));
-        }
-      },
-      onChanged: (item) => {
-        if (process.env.WEBHOOK_ON_CHANGE !== `false`) {
-          sendOnWebhook(webhookDataConstructor(item));
-        }
-      },
-    }
-  });
-
-  console.log(chalk.bgBlueBright.black(`-@ Initializing...`));
-  while (!task.initialized) {
+    // Найдем ссылки
+    let links;
     try {
-      await task.init();
+      links = await gainLinks();
     } catch (e) {
-      if (!(e instanceof TimeoutError)) {
-        console.error(e.stack);
+      throw new Error(`Check .env file`);
+    }
+
+    console.log();
+
+    // Поиск прокси
+    let proxy;
+    try {
+      proxy = await readProxyList(process.env.PROXY);
+    } catch (e) {
+      console.log(chalk.yellow(`-@@ No proxy list loaded`));
+    }
+
+    // Инициализация ботов
+    const isDiscord = process.env.BOT_SOURCE === undefined || process.env.BOT_SOURCE.toUpperCase() === `DISCORD`;
+
+    const isTelegram = !process.env.BOT_SOURCE || process.env.BOT_SOURCE.toUpperCase() === `TELEGRAM`;
+    let telegramClient: TelegramClient;
+
+    if (isTelegram) {
+      telegramClient = new TelegramClient(process.env.TELEGRAM_TOKEN, {polling: true});
+      await telegramClient.init();
+    }
+
+    const sendMessage = (item: StateItem) => {
+      if (isDiscord) {
+        sendOnWebhook(webhookDataConstructor(item));
+      } else if (isTelegram) {
+        telegramClient.sendAll(item);
+      }
+    };
+
+    const onNew = (item: StateItem) => {
+      if (process.env.MESSAGE_ON_NEW !== `false`) {
+        sendMessage(item);
+      }
+    };
+
+    const onChanged = (item: StateItem) => {
+      if (process.env.MESSAGE_ON_CHANGE !== `false`) {
+        sendMessage(item);
+      }
+    };
+
+
+    const task = new Task({
+      links,
+      proxy,
+      subscribe: {
+        onNew,
+        onChanged,
+      }
+    });
+
+    console.log(chalk.bgBlueBright.black(`-@ Initializing...`));
+    while (!task.initialized) {
+      try {
+        await task.init();
+      } catch (e) {
+        if (!(e instanceof TimeoutError)) {
+          console.error(e.stack);
+        }
       }
     }
-  }
 
-  // Бесконечный цикл сравнения состояния
-  for (; ;) {
-    try {
-      console.log();
-      console.log(chalk.bgMagentaBright.black(`-@ Updating...`));
-      await task.update();
-      console.log(`-@ Fetched in ${(task.lastIterationTime / 1000).toFixed(3)}s`);
-      console.log(`-@ Waiting for ${process.env.DELAY}ms`);
-      await waitFor(parseInt(process.env.DELAY));
-
-      checkMemory(parseInt(process.env.MEMORY_EDGE) || 200);
-    } catch (e) {
-      if (e.message === `restart`) {
-        throw e;
-      }
-
-      if (!(e instanceof TimeoutError) && !(e instanceof IpBanError)) {
-        console.error(chalk.red(e.trace || e.message));
+    // Бесконечный цикл сравнения состояния
+    for (; ;) {
+      try {
+        console.log();
+        console.log(chalk.bgMagentaBright.black(`-@ Updating...`));
+        await task.update();
+        console.log(`-@ Fetched in ${(task.lastIterationTime / 1000).toFixed(3)}s`);
         console.log(`-@ Waiting for ${process.env.DELAY}ms`);
         await waitFor(parseInt(process.env.DELAY));
+
+        checkMemory(parseInt(process.env.MEMORY_EDGE) || 200);
+      } catch (e) {
+        if (e.message === `restart`) {
+          throw e;
+        }
+
+        if (!(e instanceof TimeoutError) && !(e instanceof IpBanError)) {
+          console.error(chalk.red(e.trace || e.message));
+          console.log(`-@ Waiting for ${process.env.DELAY}ms`);
+          await waitFor(parseInt(process.env.DELAY));
+        }
       }
     }
-  }
 
-  /*
-  console.log(`-@ Type anything to exit: `);
-  await readConsole();
-  process.exit(0);
-  */
-})().catch((err) => {
+  }
+)
+().catch((err) => {
   if (err.message === `restart`) {
-    process.exit(1);
+    process.exit(2);
   }
 
-  console.error(chalk.red(err.stack));
-  console.log(`-@ Type anything to exit: `);
-  readConsole().then(() => process.exit(1)).catch(() => process.exit(1));
+
+  console.error(`\n` + err.stack);
+
+  process.exit(1);
 });
